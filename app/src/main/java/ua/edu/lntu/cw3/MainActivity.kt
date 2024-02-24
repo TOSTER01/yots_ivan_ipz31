89314967473d8b871d6144e1ef1a985c3001dae0
TOSTER01 <yots.i2504@lntu.edu.ua>
2024-02-24 13:17:23 +0200

    Second column

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -3,8 +3,12 @@ package ua.edu.lntu.cw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
@@ -15,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.cw3.ui.theme.IPZ_KR_3Theme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -38,9 +43,20 @@ data class ListItem(val title: String, val subtitle: String)
 
 @Composable
 fun ListItemView(item: ListItem) {
-    Column {
-        Text(text = item.title, fontWeight = FontWeight.Bold)
-        Text(text = item.subtitle, color = Color.Gray)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column {
+            Text(text = item.title, fontWeight = FontWeight.Bold)
+            Text(text = item.subtitle, color = Color.Gray)
+        }
+        Column {
+            Text(text = item.title, fontWeight = FontWeight.Bold)
+            Text(text = item.subtitle, color = Color.Gray)
+        }
     }
 }
 
@@ -53,6 +69,7 @@ fun VerticalScrollableList(items: List<ListItem>) {
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
