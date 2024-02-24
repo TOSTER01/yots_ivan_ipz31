c081e1c7ae22411cc845a44a8ef55c5c59be89f0
TOSTER01 <yots.i2504@lntu.edu.ua>
2024-02-24 13:09:30 +0200

    List created

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -3,12 +3,17 @@ package ua.edu.lntu.cw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import ua.edu.lntu.cw3.ui.theme.IPZ_KR_3Theme
 
@@ -29,18 +34,33 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class ListItem(val title: String, val subtitle: String)
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ListItemView(item: ListItem) {
+    Column {
+        Text(text = item.title, fontWeight = FontWeight.Bold)
+        Text(text = item.subtitle, color = Color.Gray)
+    }
+}
+
+@Composable
+fun VerticalScrollableList(items: List<ListItem>) {
+    LazyColumn {
+        items(items.size) { index ->
+            ListItemView(item = items[index])
+            Divider()
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    IPZ_KR_3Theme {
-        Greeting("Android")
-    }
+fun MyScreen() {
+    val items = listOf(
+        ListItem("Елемент 1", "Опис елементу 1"),
+        ListItem("Елемент 2", "Опис елементу 2"),
+    )
+
+    VerticalScrollableList(items = items)
 }
